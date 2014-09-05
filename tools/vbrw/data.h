c3869663d8df143934099c37901c6e7ae5252534
Charlie FG <charlie@carrotsrc.org>
2014-09-05 10:06:26 +0100

    Added a CHS data structure

--- tools/vbrw/data.h
+++ tools/vbrw/data.h
@@ -1,22 +1,31 @@
 #ifndef DATA_H
 #define DATA_H
 
+struct chs {
+	uint16_t c;
+	uint8_t h;
+	uint8_t s;
+};
+
+#define START_CHS 1
+#define END_CHS 2
+
 struct pentry {
 	uint8_t bf;
 
 	uint8_t sh;
-	uint8_t sc;
 	uint8_t ss;
+	uint8_t sc;
 
 	uint8_t id;
 
 	uint8_t eh;
-	uint8_t ec;
 	uint8_t es;
+	uint8_t ec;
 
 	uint32_t rel;
 	uint32_t total; 
-};
+} __attribute__((packed));
 
 struct mbrecord {
 	char uid[10];
@@ -24,5 +33,5 @@ struct mbrecord {
 	struct pentry p2;
 	struct pentry p3;
 	struct pentry p4;
-};
+} __attribute__((packed));
 #endif
